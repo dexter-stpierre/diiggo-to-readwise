ece07a6669da0d1d5c078b8bf855161e1e7c80ba
Dexter St Pierre <dexter.stpierre.dev@gmail.com>
2021-05-20 23:08:46 -0500

    Fetching bookmarks

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import fetch from 'node-fetch';
+import base64 from 'base-64';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 console.log('this is working');
+fetch(`https://secure.diigo.com/api/v2/bookmarks?key=${process.env.API_KEY}&count=100&user=${process.env.USERNAME}&filter=all`, {
+  headers: {
+    Authorization: `Basic ${base64.encode(`${process.env.USERNAME}:${process.env.PASSWORD}`)}`,
+  },
+}).then(async (response) => {
+  const bookmarks = await response.json();
+  console.log(bookmarks);
+  debugger;
+});
